158e64b4661de782e7f64797b95e406feb4e409a
Mika Simanainen <mika.simanainen@gmail.com>
2020-05-31 13:34:02 +0300

    remove rewiring in tests

---
+++ test/options.test.js
@@ -0,0 +1,87 @@
+const chrome = require('sinon-chrome/extensions');
+const expect = require('chai').expect;
+
+import {
+    loadOptions,
+    saveOptions,
+} from '../src/util';
+
+describe('Test options', () => {
+    before(() => {
+        global.chrome = chrome;
+    });
+
+    after(() => {
+        delete global.chrome;
+    });
+
+    beforeEach(() => {
+        chrome.flush();
+    });
+
+    it('Load default options', async () => {
+        chrome.storage.local.get.withArgs(['globals', 'servers']).yields({});
+
+        expect(chrome.storage.local.set.notCalled).to.equal(true);
+        const loadedOptions = await loadOptions();
+        expect(chrome.storage.local.get.calledOnce).to.equal(true);
+        expect(loadedOptions).to.deep.equal({
+            globals: {
+                addAdvanced: false,
+                currentServer: 0,
+                addPaused: false,
+                contextMenu: 1,
+                catchUrls: true,
+                enableNotifications: true,
+                labels: []
+            },
+            servers: [
+                {
+                    name: 'Default',
+                    application: 'biglybt',
+                    hostname: '',
+                    username: '',
+                    password: '',
+                    directories: [],
+                    clientOptions: {}
+                }
+            ]
+        });
+    });
+
+    it('Save and load custom options', async () => {
+        const modifiedOptions = {
+            globals: {
+                addAdvanced: false,
+                currentServer: 0,
+                addPaused: true,
+                contextMenu: 0,
+                catchUrls: false,
+                enableNotifications: true,
+                labels: []
+            },
+            servers: [
+                {
+                    name: 'My client',
+                    application: 'cloudtorrent',
+                    hostname: 'https://127.0.0.1/',
+                    username: '',
+                    password: '',
+                    directories: [],
+                    clientOptions: {}
+                }
+            ]
+        };
+
+        expect(chrome.storage.local.set.notCalled).to.equal(true);
+        saveOptions(modifiedOptions);
+        expect(chrome.storage.local.set.calledOnce).to.equal(true);
+
+        chrome.storage.local.get.withArgs(['globals', 'servers']).yields(modifiedOptions);
+
+        expect(chrome.storage.local.get.notCalled).to.equal(true);
+        const loadedOptions = await loadOptions();
+        expect(chrome.storage.local.get.calledOnce).to.equal(true);
+        expect(loadedOptions).to.deep.equal(modifiedOptions);
+    });
+});
--- test/util.test.js
+++ test/util.test.js
@@ -1,25 +1,17 @@
-const chrome = require('sinon-chrome/extensions');
 const expect = require('chai').expect;
 const { JSDOM } = require('jsdom');
-const rewire = require('rewire');
 
 import {getTestTorrent} from './helpers';
-import {getTorrentName} from '../src/util';
+import {
+    isTorrentUrl,
+    getTorrentName,
+    isMagnetUrl,
+    getMagnetUrlName,
+    getURL,
+} from '../src/util';
 
 describe('Test helpers', () => {
-    before(() => {
-        global.chrome = chrome;
-    });
-
-    beforeEach(() => {
-        chrome.flush();
-    });
-
-    it('isTorrentUrl(url)', () => {
-        const util = rewire('./../src/util');
-
-        const isTorrentUrl = util.__get__('isTorrentUrl');
-
+    it('isTorrentUrl', () => {
         const validUrls = [
             'https://example.com/file.torrent',
             'https://example.com/file.torrent?query=value',
@@ -34,11 +26,7 @@ describe('Test helpers', () => {
         invalidUrls.forEach((url) => expect(isTorrentUrl(url)).to.equal(false));
     });
 
-    it('isMagnetUrl(url)', () => {
-        const util = rewire('./../src/util');
-
-        const isMagnetUrl = util.__get__('isMagnetUrl');
-
+    it('isMagnetUrl', () => {
         const validUrls = [
             'magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a&dn=Test Torrent',
         ];
@@ -50,11 +38,7 @@ describe('Test helpers', () => {
         invalidUrls.forEach((url) => expect(isMagnetUrl(url)).to.equal(false));
     });
 
-    it('getMagnetUrlName(url)', () => {
-        const util = rewire('./../src/util');
-
-        const getMagnetUrlName = util.__get__('getMagnetUrlName');
-
+    it('getMagnetUrlName', () => {
         expect(getMagnetUrlName('magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a&dn=Test Torrent')).to.equal('Test Torrent');
         expect(getMagnetUrlName('magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a&dn=Test Torrent&tr=http://tracker.example.com/announce')).to.equal('Test Torrent');
 
@@ -72,108 +56,19 @@ describe('Test helpers', () => {
 
         delete global.FileReader;
     });
-});
-
-describe('Test options', () => {
-    before(() => {
-        global.chrome = chrome;
-    });
-
-    beforeEach(() => {
-        chrome.flush();
-    });
-
-    it('Load default options', async () => {
-        const util = rewire('./../src/util');
-
-        const loadOptions = util.__get__('loadOptions');
-
-        chrome.storage.local.get.withArgs(['globals', 'servers']).yields({});
-
-        expect(chrome.storage.local.set.notCalled).to.equal(true);
-        const loadedOptions = await loadOptions();
-        expect(chrome.storage.local.get.calledOnce).to.equal(true);
-        expect(loadedOptions).to.deep.equal({
-            globals: {
-                addAdvanced: false,
-                currentServer: 0,
-                addPaused: false,
-                contextMenu: 1,
-                catchUrls: true,
-                enableNotifications: true,
-                labels: []
-            },
-            servers: [
-                {
-                    name: 'Default',
-                    application: 'biglybt',
-                    hostname: '',
-                    username: '',
-                    password: '',
-                    directories: [],
-                    clientOptions: {}
-                }
-            ]
-        });
-    });
-
-    it('Save and load custom options', async () => {
-        const util = rewire('./../src/util');
-
-        const loadOptions = util.__get__('loadOptions');
-        const saveOptions = util.__get__('saveOptions');
-
-        const modifiedOptions = {
-            globals: {
-                addAdvanced: false,
-                currentServer: 0,
-                addPaused: true,
-                contextMenu: 0,
-                catchUrls: false,
-                enableNotifications: true,
-                labels: []
-            },
-            servers: [
-                {
-                    name: 'My client',
-                    application: 'cloudtorrent',
-                    hostname: 'https://127.0.0.1/',
-                    username: '',
-                    password: '',
-                    directories: [],
-                    clientOptions: {}
-                }
-            ]
-        };
-
-        expect(chrome.storage.local.set.notCalled).to.equal(true);
-        saveOptions(modifiedOptions);
-        expect(chrome.storage.local.set.calledOnce).to.equal(true);
-
-        chrome.storage.local.get.withArgs(['globals', 'servers']).yields(modifiedOptions);
-
-        expect(chrome.storage.local.get.notCalled).to.equal(true);
-        const loadedOptions = await loadOptions();
-        expect(chrome.storage.local.get.calledOnce).to.equal(true);
-        expect(loadedOptions).to.deep.equal(modifiedOptions);
-    });
 
     it('Get server url with basic auth', () => {
-
-        const util = rewire('../src/util');
-        const getURL = util.__get__('getURL');
-
         expect(getURL({ hostname: 'https://127.0.0.1:4000/' })).to.equal('https://127.0.0.1:4000/')
         expect(getURL({ hostname: 'https://127.0.0.1:4000' })).to.equal('https://127.0.0.1:4000/')
-        
-        expect(getURL({ 
-            hostname: 'https://127.0.0.1:4000/', 
+
+        expect(getURL({
+            hostname: 'https://127.0.0.1:4000/',
             username: 'foo',
             password: 'bar',
-        })).to.equal('https://foo:bar@127.0.0.1:4000/')        
-        
-        expect(getURL({ 
-            hostname: 'https://127.0.0.1:4000/', 
+        })).to.equal('https://foo:bar@127.0.0.1:4000/')
+
+        expect(getURL({
+            hostname: 'https://127.0.0.1:4000/',
             username: '',
             password: '',
         })).to.equal('https://127.0.0.1:4000/')
