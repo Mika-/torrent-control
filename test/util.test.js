5d85d9873ad75e521ebc8bc8be2613de0f314c5f
Mika Simanainen <mika.simanainen@gmail.com>
2020-07-25 15:34:31 +0300

    split options tests

--- test/util.test.js
+++ test/util.test.js
@@ -7,12 +7,14 @@ import {
     saveOptions,
 } from '../src/util.js';
 
+import {getTestTorrent} from './helpers.js';
+
 describe('Test helpers', () => {
     beforeEach(() => {
         chrome.flush();
     });
 
-    it('isTorrentUrl(url)', () => {
+    it('isTorrentUrl', () => {
         const validUrls = [
             'https://example.com/file.torrent',
             'https://example.com/file.torrent?query=value',
@@ -27,7 +29,7 @@ describe('Test helpers', () => {
         invalidUrls.forEach((url) => expect(isTorrentUrl(url)).to.equal(false));
     });
 
-    it('isMagnetUrl(url)', () => {
+    it('isMagnetUrl', () => {
         const validUrls = [
             'magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a&dn=Test Torrent',
         ];
@@ -39,100 +41,28 @@ describe('Test helpers', () => {
         invalidUrls.forEach((url) => expect(isMagnetUrl(url)).to.equal(false));
     });
 
-    it('getMagnetUrlName(url)', () => {
+    it('getMagnetUrlName', () => {
         expect(getMagnetUrlName('magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a&dn=Test Torrent')).to.equal('Test Torrent');
         expect(getMagnetUrlName('magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a&dn=Test Torrent&tr=http://tracker.example.com/announce')).to.equal('Test Torrent');
 
         expect(getMagnetUrlName('magnet:?xt=urn:btih:c12fe1c06bba254a9dc9f519b335aa7c1367a88a')).to.equal(false);
         expect(getMagnetUrlName('https://example.com/file.torrent')).to.equal(false);
     });
-});
-
-describe('Test options', () => {
-    beforeEach(() => {
-        chrome.flush();
-    });
-
-    it('Load default options', async () => {
-        chrome.storage.local.get.withArgs(['globals', 'servers']).yields({});
-
-        expect(chrome.storage.local.set.notCalled).to.equal(true);
-        const loadedOptions = await loadOptions();
-        expect(chrome.storage.local.get.calledOnce).to.equal(true);
-        expect(loadedOptions).to.deep.equal({
-            globals: {
-                addAdvanced: false,
-                currentServer: 0,
-                addPaused: false,
-                contextMenu: 1,
-                catchUrls: true,
-                enableNotifications: true,
-                labels: []
-            },
-            servers: [
-                {
-                    name: 'Default',
-                    application: 'biglybt',
-                    hostname: '',
-                    username: '',
-                    password: '',
-                    directories: [],
-                    clientOptions: {}
-                }
-            ]
-        });
-    });
-
-    it('Save and load custom options', async () => {
-        const modifiedOptions = {
-            globals: {
-                addAdvanced: false,
-                currentServer: 0,
-                addPaused: true,
-                contextMenu: 0,
-                catchUrls: false,
-                enableNotifications: true,
-                labels: []
-            },
-            servers: [
-                {
-                    name: 'My client',
-                    application: 'cloudtorrent',
-                    hostname: 'https://127.0.0.1/',
-                    username: '',
-                    password: '',
-                    directories: [],
-                    clientOptions: {}
-                }
-            ]
-        };
-
-        expect(chrome.storage.local.set.notCalled).to.equal(true);
-        saveOptions(modifiedOptions);
-        expect(chrome.storage.local.set.calledOnce).to.equal(true);
-
-        chrome.storage.local.get.withArgs(['globals', 'servers']).yields(modifiedOptions);
-
-        expect(chrome.storage.local.get.notCalled).to.equal(true);
-        const loadedOptions = await loadOptions();
-        expect(chrome.storage.local.get.calledOnce).to.equal(true);
-        expect(loadedOptions).to.deep.equal(modifiedOptions);
-    });
 
     it('Get server url with basic auth', () => {
-        expect(getURL({ hostname: 'https://127.0.0.1:4000/' })).to.equal('https://127.0.0.1:4000/')
-        expect(getURL({ hostname: 'https://127.0.0.1:4000' })).to.equal('https://127.0.0.1:4000/')
-        
-        expect(getURL({ 
-            hostname: 'https://127.0.0.1:4000/', 
+        expect(getURL({ hostname: 'https://127.0.0.1:4000/' })).to.equal('https://127.0.0.1:4000/');
+        expect(getURL({ hostname: 'https://127.0.0.1:4000' })).to.equal('https://127.0.0.1:4000/');
+
+        expect(getURL({
+            hostname: 'https://127.0.0.1:4000/',
             username: 'foo',
             password: 'bar',
-        })).to.equal('https://foo:bar@127.0.0.1:4000/')        
-        
-        expect(getURL({ 
-            hostname: 'https://127.0.0.1:4000/', 
+        })).to.equal('https://foo:bar@127.0.0.1:4000/');
+
+        expect(getURL({
+            hostname: 'https://127.0.0.1:4000/',
             username: '',
             password: '',
-        })).to.equal('https://127.0.0.1:4000/')
-    })
+        })).to.equal('https://127.0.0.1:4000/');
+    });
 });
