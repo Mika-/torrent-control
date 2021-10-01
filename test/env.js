9b7ee821f0dabc09b5f9435235290a65149f7395
Mika Simanainen <mika.simanainen@gmail.com>
2021-10-01 20:00:09 +0300

    update deps

--- test/env.js
+++ test/env.js
@@ -1,7 +1,8 @@
-import 'chai/register-expect.js';
+import {expect} from 'chai';
 import chrome from 'sinon-chrome';
 import JSDOM from 'jsdom';
 
+global.expect = expect;
 global.chrome = chrome;
 
 const jsdom = new JSDOM.JSDOM();
